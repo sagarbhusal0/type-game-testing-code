9759d035b70a8cd3bc0aeb621003209481d06831
Sagar Bhusal <127485363+sagarbhusal0@users.noreply.github.com>
2024-08-17 13:27:42 +0545

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -155,7 +155,7 @@ export default function Header() {
     return (
         <header className={timerId ? "hidden" : undefined}>
             <a href="." className="brand">
-                <img src="https://iili.io/dGWrMba.png" alt="Logo" style={{ height: "50px" }} />
+                <img src="https://iili.io/dGWrMba.png" alt="Logo" style={{ height: "70px" }} />
                
             </a>
             <div className="buttons">
