03a65c60852f10f315402fc54a905fe1b25e4ae9
Sagar Bhusal <127485363+sagarbhusal0@users.noreply.github.com>
2024-08-17 14:00:22 +0545

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,19 @@ interface AnimationProps {
 }
 
 export const options: Options = {
-    time: [15, 30, 45, 60, 120],
+    time: [  15,      // 15 seconds
+        30,      // 30 seconds
+        45,      // 45 seconds
+        60,      // 1 minute
+        120,     // 2 minutes
+        300,     // 5 minutes
+        480,     // 8 minutes
+        600,     // 10 minutes
+        900,     // 15 minutes
+        1200,    // 20 minutes
+        1500,    // 25 minutes
+        1800, // 30 minutes 
+           ],
     theme: [
         "default",
         "mkbhd",
